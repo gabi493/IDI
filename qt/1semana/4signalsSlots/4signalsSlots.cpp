9e3975093c4515767eca1aa1fea11fb30fa1425d
Gabriel C.- <gabrielcarrillolopez@gmail.com>
2014-05-14 14:49:10 +0200

    modified 4signalsSlots

--- qt/1semana/4signalsSlots/4signalsSlots.cpp
+++ qt/1semana/4signalsSlots/4signalsSlots.cpp
@@ -12,27 +12,30 @@ int main(int argc, char **argv) {
 	a.setFont(fontFamily);
 	
 	QFrame F(0, NULL);	//Crea un frame
-	
 	QHBoxLayout *cH = new QHBoxLayout(&F);			//Crea un contenidornidor horitzontal
-
-	QLineEdit *le = new QLineEdit(&F);			//Afegeix una caixa de text
-	cH->addWidget(le);
-
+	QLineEdit *le = new QLineEdit("Line Edit", &F);		//Afegeix una caixa de text
+	QLabel *label_mostra = new QLabel ("Muestra", &F);	//Afegeix un label
+	
 	QSpacerItem *sp = new QSpacerItem(100, 20);		//Afegeix un espai (horitzontal, vertical)
-	cH->addItem(sp);
-
 	QVBoxLayout *cV = new QVBoxLayout(&F);			//Crea un contrnidor vertical
+	QPushButton *ok = new QPushButton("D'acord", &F);	//Afegeix un boto
+	QPushButton *surt = new QPushButton("&Sortir", &F);	//Afegeix un altre boto
+
+
+	//Insertem el LineEdit(le), el SpacerItem(sp) i el VBoxLayout(cV) al layout horitzontal
+	cH->addWidget(le);
+	cH->addWidget(label_mostra);
+	cH->addItem(sp);
 	cH->addLayout(cV);
 
-	QPushButton *ok = new QPushButton("D'acord", &F);	//Afegeix un boto
+	//Insertem els PushButton(ok + surt) al layout vertical
 	cV->addWidget(ok);
-
-	QPushButton *surt = new QPushButton("&Sortir", &F);	//Afegeix un altre boto
 	cV->addWidget(surt);
 
-
-
-
+	//Conectem els signals amb els slots
+	a.connect(surt, SIGNAL(clicked()), F, SLOT(close()));
+	a.connect(ok, SIGNAL(clicked()), label_mostra, SLOT(setTexto()));
+	a.connect(le, SIGNAL(textChanged(const QString&)), label_mostra, SLOT(setText(const QString &)));
 
 
 	F.show();
